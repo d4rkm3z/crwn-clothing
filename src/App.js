4524aba7adaa4f723c262793ce03f1cd9416c1d4
Kirill.Sadovnikov <kirill.sadovnikov@nexign.com>
2020-09-01 02:41:13 +0300

    feat: add header to application

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 import "./App.css";
 
 export const App = () => (
   <div>
+    <Header />
     <Switch>
       <Route exact path="/" component={HomePage} />
       <Route path="/shop" component={ShopPage} />
